f7c7b42b75aedd9b65df94eab4a40c159d1eeca4
Kitwana Akil <kakil@me.com>
2023-07-10 17:17:59 -0400

    Initial Commit

---
+++ WorldTrekr/WorldTrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTrekrApp.swift
+//  WorldTrekr
+//
+//  Created by Kitwana Akil on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WorldTrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
